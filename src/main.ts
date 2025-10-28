a089f10da98469873c74572b31b3930ced9749ae
Alexandr Smotrov <98663387+Alex632S@users.noreply.github.com>
2025-10-28 21:33:03 +0300

    feature: реализована таблица с ее настройками (#1)
    Co-authored-by: Александр Смотров <aasmotrov@basistech.ru>

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
-
-import { LightTheme } from '@/utils/light'
+import ToastService from 'primevue/toastservice'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
+import { MyPreset } from '@/utils/theme'
+import '@/assets/styles/main.css'
+import Tooltip from 'primevue/tooltip'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(PrimeVue, {
   theme: {
-    preset: LightTheme,
-    options: {
-      darkModeSelector: 'light',
-      prefix: 'p',
-    },
+    preset: MyPreset,
   },
 })
+app.use(ToastService)
+app.directive('tooltip', Tooltip)
 
 app.mount('#app')
